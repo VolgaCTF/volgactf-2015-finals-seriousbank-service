8f240d38cc58f4b49b714f4d0afe1d47011c022a
Dennis Tikhonov <honeyjonny@gmail.com>
2015-09-05 23:24:10 +0400

    New database settings, cleaned repo

--- seriousbank/settings.py
+++ seriousbank/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -45,7 +44,6 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-#    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -79,8 +77,12 @@ WSGI_APPLICATION = 'seriousbank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sbankdb',
+        'USER': 'mainuser',
+        'PASSWORD': '777',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
